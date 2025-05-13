05d4347c9eb4de0f644098965ceec587f26af8fd
pottmeier <91335395+pottmeier@users.noreply.github.com>
2025-05-13 21:28:09 +0200

    embedded shorts and have them resize

--- app/food/page.tsx
+++ app/food/page.tsx
@@ -5,15 +5,35 @@ import { Video } from "@/components/video/types";
 export default function foodPage() {
   const food: Video[] = [
     {
-      title: "Amazing Afghan Entrance & Dance With Afghani Dress",
-      url: "https://www.youtube.com/watch?v=76b0SXH0vok",
+      title: "Overview over the dishes",
+      url: "https://www.youtube.com/shorts/_QMBzJ7dli0",
+    },
+    {
+      title: "Mantu",
+      url: "https://youtube.com/shorts/WJkzZPRobOs",
+    },
+    {
+      title: "Sheer Yakh",
+      url: "https://www.youtube.com/shorts/XPYgt6Xq8is",
+    },
+    {
+      title: "Shorba",
+      url: "https://www.youtube.com/shorts/oLThHU-SCY4",
+    },
+    {
+      title: "Dupiyaza",
+      url: "https://www.youtube.com/shorts/7i-LA36L8hw",
+    },
+    {
+      title: "Bolani",
+      url: "https://www.youtube.com/shorts/Z5Xg0wlVWAY",
     },
   ];
 
   return (
     <section className="text-center">
       <h1 className={title()}>Food</h1>
-      <VideoGallery videos={food} layout="stacked"></VideoGallery>
+      <VideoGallery videos={food} layout="grid"></VideoGallery>
     </section>
   );
 }
--- components/video/videocard.tsx
+++ components/video/videocard.tsx
@@ -6,19 +6,35 @@ type VideoCardProps = {
 };
 
 export default function VideoCard({ title, url }: VideoCardProps) {
+  function getYouTubeVideoId(url: String) {
+    if (url.includes("youtu.be/")) {
+      return url.split("youtu.be/")[1].split(/[?&]/)[0];
+    }
+
+    if (url.includes("youtube.com/watch")) {
+      return url.split("v=")[1]?.split("&")[0];
+    }
+
+    if (url.includes("youtube.com/shorts/")) {
+      return url.split("shorts/")[1].split(/[?&]/)[0];
+    }
+
+    return null;
+  }
   // Extract the YouTube video ID from the URL
-  const videoId = url.split("v=")[1]?.split("&")[0];
+  const isShort = url.includes("youtube.com/shorts/");
+  const videoId = getYouTubeVideoId(url);
 
   return (
     <Card fullWidth={true} className="w-full max-w-4xl mx-auto">
       <CardHeader>{title}</CardHeader>
-      <CardBody className="relative pt-[56.25%]">
+      <CardBody className={isShort ? "relative pt-[177.78%]" : "relative pt-[56.25%]"}>
         {" "}
         {/* This keeps the 16:9 aspect ratio */}
         <iframe
           title={title}
           className="absolute top-0 left-0 w-full h-full"
-          src={`https://www.youtube.com/embed/${videoId}`}
+          src={`https://www.youtube.com/embed/${videoId}?modestbranding=1&rel=0&autohide=1`}
           frameBorder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
